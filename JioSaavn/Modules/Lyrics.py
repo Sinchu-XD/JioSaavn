3bb384e4927003db62fba3ad36dc4714c4ab132f
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-04-08 09:18:45 +0530

    JioSaavn API

--- JioSaavn/Modules/Lyrics.py
+++ JioSaavn/Modules/Lyrics.py
@@ -2,9 +2,14 @@ from ..Core.Request import Request
 from .. import endpoints
 
 
-async def get_lyrics(song_id):
-    async with Request() as req:
-        data = await req.get(endpoints.LYRICS + song_id)
+async def get_lyrics(song_id, client=None):
+    url = endpoints.LYRICS + song_id
+
+    if client:
+        data = await client.get(url)
+    else:
+        async with Request() as req:
+            data = await req.get(url)
 
     if not data:
         return None

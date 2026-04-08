fa852c4c5d73251da7f8a72b8673b8d381085c2b
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-04-08 09:15:48 +0530

    JioSaavn API

--- JioSaavn/Modules/Playlist.py
+++ JioSaavn/Modules/Playlist.py
@@ -4,9 +4,14 @@ from ..Formatter.Playlist import format_playlist
 from .Lyrics import get_lyrics
 
 
-async def get_playlist(list_id):
-    async with Request() as req:
-        data = await req.get(endpoints.PLAYLIST + list_id)
+async def get_playlist(list_id, client=None):
+    url = endpoints.PLAYLIST + list_id
+
+    if client:
+        data = await client.get(url)
+    else:
+        async with Request() as req:
+            data = await req.get(url)
 
     if not data:
         return None

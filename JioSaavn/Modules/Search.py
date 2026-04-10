b28c227c0cda743e362c7886f51738369cbaf858
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-04-10 17:55:32 +0530

    JioSaavn API

--- JioSaavn/Modules/Search.py
+++ JioSaavn/Modules/Search.py
@@ -1,10 +1,13 @@
 import urllib.parse
+import asyncio
+
 from ..Core.Request import Request
 from .. import endpoints
-from .Song import get_song
+from ..Formatter.Song import format_song
+from .Lyrics import get_lyrics
 
 
-async def search(query, full=False, client=None):
+async def search(query, limit=10, lyrics=False, client=None):
     query = urllib.parse.quote(query)
     url = endpoints.SEARCH + query
 
@@ -18,12 +21,14 @@ async def search(query, full=False, client=None):
         return []
 
     songs = data.get("results") or data.get("songs", {}).get("data", [])
+    songs = songs[:limit]
 
-    if not full:
-        return songs
+    async def process(song):
+        return await format_song(
+            song,
+            get_lyrics if lyrics else None
+        )
 
-    return [
-        await get_song(s.get("id"), client=client)
-        for s in songs
-        if s.get("id")
-    ]
+    return await asyncio.gather(*[
+        process(s) for s in songs if s.get("id")
+    ])
